6cf2d53a17a830df6a8d7f5c596a16d1a7f7d909
fjrois <fjroissiso@gmail.com>
2021-11-19 22:08:30 -0800

    Basic routes

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import config from './config.js';
+import tasksRouter from './routers/tasks.js';
+// import panelsRouter from './routers/panels.js';
+// import topicsRouter from './routers/topics.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('It works');
 });
+
+app.use('/tasks', tasksRouter);
+// app.use('/panels', panelsRouter);
+// app.use('/topics', topicsRouter);
 
 const port = 3000;
 console.log(`Server listening on port ${port}`);
