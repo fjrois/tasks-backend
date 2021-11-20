59b9be87ea4303b25e97076afff08a414bf7f6ce
fjrois <fjroissiso@gmail.com>
2021-11-19 22:45:05 -0800

    Fixed port for deployment

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use('/tasks', tasksRouter);
 // app.use('/panels', panelsRouter);
 // app.use('/topics', topicsRouter);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 console.log(`Server listening on port ${port}`);
 app.listen(port);
