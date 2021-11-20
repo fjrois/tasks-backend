cb913b208ab5abf061b6db99f109958fa91c5c27
fjrois <fjroissiso@gmail.com>
2021-11-19 22:08:14 -0800

    Firebase config

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: 'tasks-f3816.firebaseapp.com',
+    projectId: 'tasks-f3816',
+    storageBucket: 'tasks-f3816.appspot.com', // Not required for Firestore
+    messagingSenderId: '979516677467', // Not required for Firestore
+    appId: '1:979516677467:web:fd282e8de7d88f98d18e7e', // Not required for Firestore
+    measurementId: 'G-NE37LF7LZ6', // Not required for Firestore
+  },
+};
+
+export default config;
